8296b26b3cf94bcb5342deac95ddd730d4009604
a.matviichuk <a.matviichuk@eska.global>
2019-12-10 17:10:03 +0200

    Apply design to DDKBuyPopup

--- frontend/src/app/component/DDKBuyPopup/index.tsx
+++ frontend/src/app/component/DDKBuyPopup/index.tsx
@@ -12,11 +12,32 @@ const DDKBuyPopup: React.FC<IDDKBuyPopupProps> = ({ }) => {
     const modalContext = React.useContext(ModalContext);
     const close = () => modalContext.close();
 
+    const go = (url: string) => {
+        alert("Open external link: " + url);
+        close();
+    };
+
     return (
         <div className={classes.root} >
-             <h3>Buy DDK popup</h3>
-             <p>TODO: Do design here</p>
-             <Button className={classes.button} onClick={close}>Ok</Button>
+             <h3 className={classes.h3}>Please choose exchanger</h3>
+             <p className={classes.p}>Lorem ipsum uipsum, Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+             <div className={classes.logos}>
+                <Button className={classes.button} onClick={()=>go("/")}>
+                    <img src="/public/img/logo-simex.png" alt="Simex" />
+                </Button>
+                <Button className={classes.button} onClick={()=>go("/")}>
+                    <img src="/public/img/logo-dobi.png" alt="Dobi" />
+                </Button>
+                <Button className={classes.button} onClick={()=>go("/")}>
+                    <img src="/public/img/logo-exmarkets.png" alt="Exmarkets" />
+                </Button>
+                <Button className={classes.button} onClick={()=>go("/")}>
+                    <img src="/public/img/logo-vindax.png" alt="Vindax" />
+                </Button>
+                <Button className={classes.button} onClick={()=>go("/")}>
+                    <img src="/public/img/logo-finexbox.png" alt="Finexbox" />
+                </Button>
+            </div>
         </div>
     );
 };
--- frontend/src/app/component/DDKBuyPopup/style.ts
+++ frontend/src/app/component/DDKBuyPopup/style.ts
@@ -2,9 +2,32 @@ import { makeStyles, Theme } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme: Theme) => ({
     root: {
-        padding: '4ex 7ex',
+        padding: '25px',
+        color: 'white',
+        backgroundColor: '#1B2033',
+        width: '500px',
+        border: 'none',
+        borderRadius: '0',
+    },
+    h3: {
+        textAlign: 'center',
+    },
+    p: {
+        margin: '2ex 0',
+    },
+    logos: {
+        textAlign: 'center',
     },
     button: {
-        background: 'lightblue',
+        display: 'inline-block',
+        background: 'white',
+        height: '56px',
+        width: '192px',
+        border: '2px solid #3D90E3',
+        padding: '8px 16px',
+        margin: '12px',
+        '&:hover': {
+            background: 'white',
+        }
     }
 }));
