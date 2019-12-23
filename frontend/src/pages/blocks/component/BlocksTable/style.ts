42bd7087f6d6dd901d5b41d782e652cdc003e8e7
Bohdan Herasymov <b.herasymov@eska.global>
2019-12-23 15:32:03 +0200

    add(theme): added theme colors and pallete

--- frontend/src/pages/blocks/component/BlocksTable/style.ts
+++ frontend/src/pages/blocks/component/BlocksTable/style.ts
@@ -1,5 +1,4 @@
 import { makeStyles, Theme } from '@material-ui/core/styles';
-import { COLORS, THEME_TYPOGRAPHY } from 'src/app/component/Theme/theme';
 
 export const useStyles = makeStyles((theme: Theme) => ({
     root: {
